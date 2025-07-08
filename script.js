2100fa279f30d17417f639937443888ec3fadfbe
Rohail Sheikh <86629893+Antendis@users.noreply.github.com>
2025-07-08 22:17:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function changeText() {
-  document.getElementById("demo").innerText = "You clicked the button!";
-}
+const reveals = document.querySelectorAll('.reveal');
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('active');
+    }
+  });
+}, { threshold: 0.1 });
+
+reveals.forEach(r => observer.observe(r));
